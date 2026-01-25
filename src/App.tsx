173dbec1b84f9db1ef3f0d1ab54b95d293e1625b
shruthik shetty <shrutikshetty14@gmail.com>
2026-01-25 18:50:15 +0530

    add simple mesh

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
-    <div>
-      <p className="text-2xl font-bold text-black">Home</p>
+    <div className="w-screen h-screen">
+      <Canvas
+        camera={{
+          position: [3, 3, 3],
+        }}
+      >
+        <color attach="background" args={["black"]} />
+        <OrbitControls />
+        <mesh>
+          <boxGeometry args={[0.5, 0.5, 0.5]} />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
