7e47945c406d8e7c3ab48a4966cbe55aca0f491c
shruthik shetty <shrutikshetty14@gmail.com>
2026-01-26 00:01:27 +0530

    helpers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
-import { FirstPersonControls } from "@react-three/drei";
+import {
+  OrbitControls,
+  GizmoHelper,
+  GizmoViewport,
+  // GizmoViewcube,
+} from "@react-three/drei";
 import AnimatedBox from "./components/animated-box";
 
 function App() {
   return (
     <div className="w-screen h-screen">
       <Canvas>
-        <color attach="background" args={["gray"]} />
-        {/* <OrbitControls /> */}
-        <FirstPersonControls movementSpeed={5} />
+        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+          <GizmoViewport />
+          {/* <GizmoViewcube /> */}
+        </GizmoHelper>
+        {/* shows the axis*/}
+        <axesHelper args={[10]} />
+        {/* shows the grid*/}
+        {/* [length , divisions , color of center lines , color of lines] */}
+        <gridHelper args={[10, 10, "red", "blue"]} />
+        <color attach="background" args={["white"]} />
+        <OrbitControls />
         <AnimatedBox />
         <directionalLight position={[2, 5, 1]} />
       </Canvas>
